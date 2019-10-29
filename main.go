7aaeab87b6ac5951933721f74622d5d9118ebdf7
nispelm <47509426+nispelm@users.noreply.github.com>
2019-10-29 09:27:48 -0700

    this has more text

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World and All"}
+		res := &response{Message: "Hello World and All more text"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
